21aa94a645bb80999c25f6b082f9e5dc0178d621
Isaac Alencar <alencarisaac3@gmail.com>
2022-12-30 11:30:35 -0300

    setup basic folder structure

---
+++ src/styles/pages/homepage.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #242424;
+  color: white;
+  font-family: sans-serif;
+  height: 100%;
+
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
