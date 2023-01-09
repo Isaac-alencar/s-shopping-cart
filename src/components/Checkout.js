d30e24f03ab78e019485b5a3900fc1d8fa04a055
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-01-09 17:54:38 -0300

    create homepage

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,12 +1,12 @@
-import styled from "styled-components";
-import { Text } from "./Text";
+import styled from 'styled-components';
+import { Text } from './Text';
 
 const Item = (props) => {
   return (
     <ItemContainer>
-      <Image src="https://placekitten.com/g/90" alt="Kitty" />
+      <Image src='https://placekitten.com/g/90' alt='Kitty' />
       <Description>
-        <Name fontSize="1.2rem" fontWeight="bold">
+        <Name fontSize='1.2rem' fontWeight='bold'>
           {props.name}
         </Name>
         <Quantity>Quantity: {props.quantity}</Quantity>
@@ -14,10 +14,10 @@ const Item = (props) => {
       </Description>
       <ActionButtons>
         <button>
-          <i class="lni lni-plus"></i>
+          <i class='lni lni-plus'></i>
         </button>
         <button>
-          <i class="lni lni-minus"></i>
+          <i class='lni lni-minus'></i>
         </button>
       </ActionButtons>
     </ItemContainer>
@@ -26,26 +26,67 @@ const Item = (props) => {
 
 export const Checkout = () => {
   return (
-    <Container>
-      <ItemsList>
-        <Item price={40} quantity={4} name="banana" />
-        <Item price={40} quantity={4} name="banana" />
-      </ItemsList>
-      <DiscountContainer>
-        <Input type="text" placeholder="Discount code" />
-        <Button>Apply</Button>
-      </DiscountContainer>
-    </Container>
+    <BigContainer>
+      <Container>
+        <TitleContainer>
+          <Text fontSize='2rem' fontWeight='bold'>
+            Shopping Cart
+          </Text>
+        </TitleContainer>
+        <ItemsList>
+          <Item price={40} quantity={4} name='banana' />
+          <Item price={40} quantity={4} name='banana' />
+        </ItemsList>
+        <DiscountContainer>
+          <Input type='text' placeholder='Discount code' />
+          <Button>Apply</Button>
+        </DiscountContainer>
+        <PriceContainer>
+          <Text>Subtotal</Text>
+          <Text>$ 230,00</Text>
+        </PriceContainer>
+        <PriceContainer>
+          <Text>Shipping</Text>
+          <Text>$ 30,00</Text>
+        </PriceContainer>
+        <PriceContainer>
+          <Text>Discount</Text>
+          <Text>$ 1,00</Text>
+        </PriceContainer>
+        <PriceContainer>
+          <Text fontWeight='bold'>Total</Text>
+          <Text fontWeight='bold'>$ 230,00</Text>
+        </PriceContainer>
+      </Container>
+
+      <CheckoutButton>Checkout</CheckoutButton>
+    </BigContainer>
   );
 };
 
-export const Container = styled.div`
+const BigContainer = styled.div`
   grid-area: checkout;
   min-width: 32rem;
   margin: 3rem 2rem;
-  padding: 2rem;
+`;
+
+export const Container = styled.div`
   background-color: #d8d8d8;
   border-radius: 12px;
+  height: fit-content;
+  margin-bottom: 2rem;
+`;
+
+// TitleContainer
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1.5rem;
+  background-color: #999999;
+
+  border-top-left-radius: inherit;
+  border-top-right-radius: inherit;
 `;
 
 // Items on cart
@@ -54,6 +95,7 @@ const ItemsList = styled.div`
   flex-direction: column;
   justify-content: space-between;
   gap: 1rem;
+  padding: 2rem 2rem 0;
 `;
 const ItemContainer = styled.div`
   display: flex;
@@ -70,8 +112,8 @@ const Image = styled.img`
 const Description = styled.div`
   display: grid;
   grid-template-areas:
-    "name name"
-    "quantity price";
+    'name name'
+    'quantity price';
   grid-template-columns: auto 1fr;
   width: 100%;
   padding: 0.75rem 0.5rem;
@@ -119,7 +161,7 @@ const ActionButtons = styled(Text)`
 const DiscountContainer = styled.div`
   display: flex;
   gap: 1rem;
-  padding: 2rem 0;
+  padding: 2rem;
 `;
 const Input = styled.input`
   width: 100%;
@@ -139,4 +181,33 @@ const Button = styled.button`
   font-weight: bold;
   background-color: #333;
   color: #fff;
+
+  cursor: pointer;
+`;
+
+const CheckoutButton = styled.button`
+  border: 0;
+  border-radius: 12px;
+  width: 100%;
+  padding: 1.4rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  background-color: #333;
+  color: #fff;
+
+  text-transform: uppercase;
+
+  cursor: pointer;
+`;
+
+// PriceContainer
+
+const PriceContainer = styled.div`
+  padding: 1.6rem 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  border-top: 0.1px solid #9999;
 `;
